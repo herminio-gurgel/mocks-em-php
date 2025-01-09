82e61e14d1ea72e12d4857138b10a48bf403d3c1
Hermíno Carlos Gurgel Medeiros Netto <herminio.gurgel@gmail.com>
2025-01-09 10:43:16 -0300

    1.4 Encerrando leilões

--- src/Service/Encerrador.php
+++ src/Service/Encerrador.php
@@ -6,16 +6,28 @@ use Alura\Leilao\Dao\Leilao as LeilaoDao;
 
 class Encerrador
 {
+    private $dao;
+
+    public function __construct(LeilaoDao $dao)
+    {
+        $this->dao = $dao;
+    }
+
     public function encerra()
     {
-        $dao = new LeilaoDao();
-        $leiloes = $dao->recuperarNaoFinalizados();
+        $leiloes = $this->dao->recuperarNaoFinalizados();
 
         foreach ($leiloes as $leilao) {
             if ($leilao->temMaisDeUmaSemana()) {
                 $leilao->finaliza();
-                $dao->atualiza($leilao);
+                $this->dao->atualiza($leilao);
             }
         }
     }
 }
+
+/*
+ * 1.6
+ *
+ * O LeilaoDao passa ser injetado no construtor
+ */
